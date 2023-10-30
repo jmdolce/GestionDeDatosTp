344f095af44756f1e2973074c1532f1424747399
Franco Centurion <frcenturion@frba.utn.edu.ar>
2023-10-30 19:50:37 -0300

    Arreglamos el orden en el exec procedures

--- exec_procedures.sql
+++ exec_procedures.sql
@@ -40,55 +40,10 @@ EXEC LOS_GDDS.MIGRAR_Alquiler --OK
 EXEC LOS_GDDS.MIGRAR_DetalleAlquiler -- hay filas con muchos NULLS
 EXEC LOS_GDDS.MIGRAR_PagoAlquiler --OK
 
+EXEC LOS_GDDS.MIGRAR_Comprador  
 EXEC LOS_GDDS.MIGRAR_Venta --OK
 EXEC LOS_GDDS.MIGRAR_PagoVenta
-EXEC LOS_GDDS.MIGRAR_Comprador  
 
 
 
-
-
--- SELECT * from LOS_GDDS.Provincia
--- SELECT * from LOS_GDDS.Localidad
--- SELECT * from LOS_GDDS.Barrio
--- SELECT * from LOS_GDDS.Sucursal
-
--- SELECT * from LOS_GDDS.Agente
-
--- SELECT * from LOS_GDDS.Alquiler
--- SELECT * from LOS_GDDS.Detalle_alquiler
--- SELECT * from LOS_GDDS.Pago_alquiler
-
--- SELECT * from LOS_GDDS.Anuncio
--- SELECT * from LOS_GDDS.Venta
-
-
--- SELECT * from LOS_GDDS.Propietario
--- SELECT * from LOS_GDDS.Comprador
--- SELECT * from LOS_GDDS.Disposicion
-
--- SELECT * from LOS_GDDS.Tipo_inmueble
-
--- SELECT * from LOS_GDDS.Estado_inmueble
--- SELECT * from LOS_GDDS.Orientacion
-
--- SELECT * from LOS_GDDS.Estado_anuncio
-
-
--- SELECT * from LOS_GDDS.Operacion
--- SELECT * from LOS_GDDS.Tipo_periodo
--- SELECT * from LOS_GDDS.Moneda
--- SELECT * from LOS_GDDS.Medio_pago
-
-
--- SELECT * from LOS_GDDS.Pago_venta
--- SELECT * from LOS_GDDS.Detalle_alquiler
-
--- SELECT * from LOS_GDDS.Inquilino
--- SELECT * from LOS_GDDS.Estado_alquiler
-
-
-
-
--- SELECT * from gd_esquema.Maestra
 
