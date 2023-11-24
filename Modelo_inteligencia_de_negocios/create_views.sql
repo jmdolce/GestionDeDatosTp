134a0e5a878fd15fdc4dadfe997ad0c25c830d9b
andydal2002 <57413612+andydal2002@users.noreply.github.com>
2023-11-24 15:35:52 -0300

    correccion de la vista 1

--- Modelo_inteligencia_de_negocios/create_views.sql
+++ Modelo_inteligencia_de_negocios/create_views.sql
@@ -9,7 +9,7 @@ SELECT
 	u.barrio, 
 	i.descripcion AS [inmueble], 
 	iamb.ambiente_id [Ambiente], 
-	DATEDIFF(day, a.fecha_publicacion, a.fecha_finalizacion) AS [Duracion dias]
+	AVG(DATEDIFF(day, a.fecha_publicacion, a.fecha_finalizacion)) AS [Duracion dias]
 FROM LOS_GDDS.BI_Anuncio a 
 JOIN LOS_GDDS.BI_Tiempo t ON t.id = a.tiempo_id
 JOIN LOS_GDDS.BI_Tipo_Operacion op ON a.operacion_id = op.id
@@ -17,7 +17,7 @@ JOIN LOS_GDDS.BI_Inmueble i ON i.id = a.inmueble_id
 JOIN LOS_GDDS.BI_Ubicacion u ON u.id = i.ubicacion_id 
 JOIN LOS_GDDS.BI_Inmueble_Ambiente iamb ON iamb.inmueble_id = i.id
 
-GROUP BY t.anio, t.cuatrimestre, op.nombre, u.barrio, iamb.ambiente_id, i.descripcion, a.fecha_publicacion, a.fecha_finalizacion
+GROUP BY t.anio, t.cuatrimestre, op.nombre, u.barrio, iamb.ambiente_id, i.descripcion
 HAVING t.anio = YEAR(GETDATE()) AND t.cuatrimestre = (MONTH(GETDATE()) - 1) / 4 + 1 -- que sea de este cuatrimestre
 
 --- VISTA 2 --- 
